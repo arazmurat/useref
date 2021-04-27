15a55043ae36b6303d17d0b4ee88bb3633862f7f
Arazmyrat <arazmirat@icloud.com>
2021-04-27 22:10:22 +0400

    added if else

--- src/reducers/dogReducer.js
+++ src/reducers/dogReducer.js
@@ -1,12 +1,28 @@
 export const dogReducer = (state, action) => {
-	switch (action.type) {
-		case "FETCH_START":
-			return { ...state, data: "", loading: true, error: "" };
-		case "FETCH_SUCCESS":
-			return { ...state, data: action.payload, loading: false };
-		case "FETCH_ERROR":
-			return { ...state, error: action.payload, loading: false };
-		default:
-			return state;
-	}
-};
+  console.log(action)  
+
+  if (action.type == "FETCH_START") {
+    return { ...state, data: "", loading: true, error: "" };
+  } 
+  else if (action.type == "FETCH_SUCCESS") {
+    return { ...state, data: action.payload, loading: false };
+  } 
+  else if (action.type == "FETCH_ERROR") {
+    return { ...state, error: action.payload, loading: false };
+  } 
+  else {
+    return state;
+  } 
+
+/*
+  switch (action.type) {
+      case "FETCH_START":
+        return { ...state, data: "", loading: true, error: "" };
+      case "FETCH_SUCCESS":
+        return { ...state, data: action.payload, loading: false };
+      case "FETCH_ERROR":
+        return { ...state, error: action.payload, loading: false };
+      default:
+        return state;
+    }*/
+  };
